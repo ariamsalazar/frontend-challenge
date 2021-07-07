902cea0dba74aa3815e443d2f9d0d4d85ee62f63
ariamsalazar <ariamsalazar@gmail.com>
2021-07-07 12:29:36 -0400

    New Branch Testing

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 
 function App(): JSX.Element {
-	return <div className="App"> Homepage Testing Error </div>;
+	return <div className="App"> Homepage Testing Develop </div>;
 }
 
 export default App;
